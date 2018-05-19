f68e7d3c48a13da4cb96f1919d7942c81c2bc655
Wykthor <wykthor.g@wgvale.com>
2018-05-18 17:34:50 -0700

    First working version, top score is 66% win rate, which is also the max

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class Apresentador():
     def reiniciar(self):
         self.portas = [0,0,0]
         self.portaCerta = randint(0,2)
+
     def abrirPorta(self,escolhida):
         if self.portaCerta == escolhida:
             abrir = randint(0,2)
@@ -40,20 +41,19 @@ class Agente():
         self.taxaDeAprendizagem = 0.01
         self.amortizacao = 0.9
 
+    def randomizar(self):
+        self.trocar = random()
+        self.naoTrocar = random()
+
     def trocarPorta(self,portas):
-        novaEscolha = randint(0,2)
-        while novaEscolha==self.escolha and portas[novaEscolha]==1:
-            novaEscolha = randint(0,2)
+        indicePortas = range(3)
+        for i in indicePortas:
+            if(i != self.escolha and portas[i]!= 1):
+                novaEscolha = i
         self.escolha = novaEscolha
 
-    def funcaoValorAcao(self,acao): #usado para retornar a expectativa de ganho de uma dada ação
-        if acao==1:
-            return self.trocar
-        elif acao==0:
-            return self.naoTrocar
-
     def escolherAcao(self,portas):
-        if(self.funcaoValorAcao(1) > self.funcaoValorAcao(0)):
+        if(self.trocar > self.naoTrocar):
             self.trocarPorta(portas)
             self.troquei = 1
         #Se a esperança de ganho por trocar for maior que
@@ -67,9 +67,9 @@ class Agente():
                 self.naoTrocar+=self.naoTrocar+self.taxaDeAprendizagem*(1+self.amortizacao*max(self.trocar,self.naoTrocar)-self.naoTrocar)
         else:
             if(self.troquei):
-                self.trocar+=self.trocar+self.taxaDeAprendizagem*(0+self.amortizacao*max(self.trocar,self.naoTrocar)-self.trocar)
+                self.trocar+=self.trocar+self.taxaDeAprendizagem*(-1+self.amortizacao*max(self.trocar,self.naoTrocar)-self.trocar)
             else:
-                self.naoTrocar+=self.naoTrocar+self.taxaDeAprendizagem*(0+self.amortizacao*max(self.trocar,self.naoTrocar)-self.naoTrocar)
+                self.naoTrocar+=self.naoTrocar+self.taxaDeAprendizagem*(-1+self.amortizacao*max(self.trocar,self.naoTrocar)-self.naoTrocar)
         if(self.trocar>1):
             self.trocar = 1
         if(self.naoTrocar>1):
@@ -84,23 +84,27 @@ def main():
     apresentador = Apresentador()
     jogador = Agente()
     vitorias = 0
+    derrotas = 0
     jogos = 0
     for i in range(100):
-        #jogador escolhe aleatoriamente que porta ele quer.
-        print(apresentador.portas,apresentador.portaCerta,jogador.escolha)
-        apresentador.abrirPorta(jogador.escolha)
-        print(apresentador.portas,apresentador.portaCerta)
-        #Apresentador abre uma porta e oferece a chance de
-        #trocar de porta ao jogador
-        jogador.escolherAcao(apresentador.portas)
-        jogador.atualizarValores(apresentador.ganhou(jogador.escolha))
-        if(apresentador.ganhou(jogador.escolha)):
-            vitorias += 1
-        jogos+=1
-        if(vitorias):
-            print("Até agora, ganhei {0:.0f}% das vezes".format(jogos/vitorias))
-        apresentador.reiniciar()
-    print(jogador.trocar,jogador.naoTrocar)
+        jogador.randomizar()
+        for i in range(10000):
+            #jogador escolhe aleatoriamente que porta ele quer.
+            apresentador.abrirPorta(jogador.escolha)
+            #Apresentador abre uma porta e oferece a chance de
+            #trocar de porta ao jogador
+            jogador.escolherAcao(apresentador.portas)
+            jogador.atualizarValores(apresentador.ganhou(jogador.escolha))
+            if(apresentador.ganhou(jogador.escolha)):
+                vitorias += 1
+                #print("Ganhei!")
+            else:
+                derrotas += 1
+                #print("Merda.")
+            jogos+=1
+            apresentador.reiniciar()
+        print("Ganhei {0:.2f}% das vezes".format(100*(float(vitorias)/float(jogos))))
+        print(jogador.trocar,jogador.naoTrocar)
     return 0
 #main
 
